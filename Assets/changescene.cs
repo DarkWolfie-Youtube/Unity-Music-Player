decbd5fe2f353e7c44826793735bdde2f3cbae48
darkwolfie37 <darkwolfiefiverr@gmail.com>
2022-04-28 11:41:39 -0400

    Added all unity files

---
+++ Assets/changescene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour
+{ 
+
+    public void MoveTOScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+
+}
